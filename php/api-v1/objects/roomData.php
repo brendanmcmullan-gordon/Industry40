86b2c5b4116798ae1c83ac498e0e7897f84b9501
Steve Gale <sgale@gordontafe.edu.au>
2019-05-03 09:38:46 +1000

    added api read php and object method

--- php/api-v1/objects/roomData.php
+++ php/api-v1/objects/roomData.php
@@ -18,4 +18,34 @@ class RoomData{
     public function __construct($db){
         $this->conn = $db;
     }
+
+    // read roomData
+    function read(){
+ 
+        // select all query
+        $query = "SELECT
+                roomName, humidity, temperature, outsideTemp, CO2, readDate 
+            FROM
+                " . $this->table_name . "
+            ORDER BY
+                readDate DESC";
+ 
+    // prepare query statement
+        $stmt = $this->conn->prepare($query);
+ 
+    // execute query
+        $stmt->execute();
+ 
+        return $stmt;
+    }
 }
+
+//  CREATE TABLE roomData (
+//      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+//      roomName VARCHAR(20) NOT NULL,
+//      humidity VARCHAR(8),
+//      temperature VARCHAR(8),
+//      outsideTemp VARCHAR(4),
+//      CO2 VARCHAR(8),
+//      readDate TIMESTAMP
+//  )
